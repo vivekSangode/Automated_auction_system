7c5fd7bbea8b5efa1814917418f9e12a819c7d1e
vivek sangode <vivekusangode@gmail.com>
2023-02-24 01:19:26 +0530

    day-3

--- masai_auction_system/src/DAO/BuyerDaoImpl.java
+++ masai_auction_system/src/DAO/BuyerDaoImpl.java
@@ -1,13 +1,20 @@
 package DAO;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import DTO.Buyer;
 import DTO.BuyerImpl;
+import DTO.SearchBuyer;
+import DTO.SearchBuyerImpl;
+import DTO.SoldItems;
+import DTO.SoldItemsImpl;
 import Exception.BuyerException;
 import Exception.CredentialException;
 import utility.DBUtils;
@@ -43,14 +50,55 @@ public class BuyerDaoImpl implements BuyerDao{
 
 	@Override
 	public String RegisterBuyer(Buyer buyer) throws BuyerException {
-		// TODO Auto-generated method stub
-		return null;
+		String result="Not Registered-Bad Details (Enter Unique Email)";
+
+        try(Connection conn=DBUtils.connectToDatabase()) {
+            PreparedStatement ps=conn.prepareStatement("insert into buyer (buyerName,email,password,location) values (?,?,?,?)");
+
+            ps.setString(1, buyer.getBuyerName());
+            ps.setString(2, buyer.getEmail());
+            ps.setString(3, buyer.getPassword());
+            ps.setString(4, buyer.getLocation());
+
+           int x= ps.executeUpdate();
+           if(x>0){
+               result="Registered Successfully";
+           }else{
+               throw new BuyerException(result);
+           }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return result;
 	}
 
 	@Override
 	public String BuyItem(int buyerId, LocalDate date, String productName) throws BuyerException {
-		// TODO Auto-generated method stub
-		return null;
+		String result = null;
+
+        try(Connection conn=DBUtils.connectToDatabase()) {
+            PreparedStatement ps=conn.prepareStatement("update products set status='sold',buyerId=?,date=? where productName=?");
+
+            ps.setInt(1, buyerId);
+            ps.setDate(2, Date.valueOf(date));
+            ps.setString(3, productName);
+
+
+            int x= ps.executeUpdate();
+            if(x>0){
+                result="Item Bought Successfully- Will be delivered shortly";
+            }else{
+                throw new BuyerException("No Product found with productName- "+productName );
+            }
+
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return result;
 	}
 	
 	public static void main(String[] args) {
@@ -64,5 +112,73 @@ public class BuyerDaoImpl implements BuyerDao{
 		}
 		
 	}
+
+	@Override
+	public List<SoldItems> searchItemByCategory(String categoryName) throws BuyerException {
+		List<SoldItems> list=new ArrayList<>();
+
+        try(Connection conn=DBUtils.connectToDatabase()) {
+
+            PreparedStatement ps=conn.prepareStatement("select p.productId,p.productName,s.sellerName,c.categoryName,p.price,p.status from products p " +
+                    "Inner Join category c Inner Join seller s " +
+                    "On p.categoryId=c.categoryId and p.sellerId=s.sellerId " +
+                    "where status='available' and c.categoryName=?");
+
+            ps.setString(1,categoryName);
+
+            ResultSet rs=ps.executeQuery();
+            while (rs.next()){
+                SoldItems soldItemsDTO=new SoldItemsImpl();
+                soldItemsDTO.setProductId(rs.getInt("productId"));
+                soldItemsDTO.setProductName(rs.getString("productName"));
+                soldItemsDTO.setSellerName(rs.getString("sellerName"));
+                soldItemsDTO.setCategoryName(rs.getString("categoryName"));
+                soldItemsDTO.setPrice(rs.getInt("price"));
+                list.add(soldItemsDTO);
+            }
+            if(list.size()==0){
+                throw new BuyerException("No Item found in Product List/ Category not found");
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+            throw new BuyerException(e.getMessage());
+        }
+        return list;
+	}
+
+	@Override
+	public List<SearchBuyer> ViewAllBuyersDetails(String categoryName) throws BuyerException {
+		 List<SearchBuyer> list=new ArrayList<>();
+
+	        try(Connection conn=DBUtils.connectToDatabase()) {
+
+	            PreparedStatement ps=conn.prepareStatement("select b.buyerId,b.buyerName,b.email,p.productName,c.categoryName,s.sellerName,p.price from products p " +
+	                    "Inner Join category c Inner Join seller s Inner Join buyer b " +
+	                    "On p.categoryId=c.categoryId and p.sellerId=s.sellerId and p.buyerId=b.buyerId " +
+	                    "where c.categoryName=? and status='sold'");
+
+	            ps.setString(1,categoryName);
+
+	            ResultSet rs=ps.executeQuery();
+	            while (rs.next()){
+	                SearchBuyer searchBuyerDTO = new SearchBuyerImpl();
+	                searchBuyerDTO.setBuyerId(rs.getInt("buyerId"));
+	                searchBuyerDTO.setBuyerName(rs.getString("buyerName"));
+	                searchBuyerDTO.setEmail(rs.getString("email"));
+	                searchBuyerDTO.setProductName(rs.getString("productName"));
+	                searchBuyerDTO.setPrice(rs.getInt("price"));
+	                list.add(searchBuyerDTO);
+	            }
+	            if(list.size()==0){
+	                throw new BuyerException("No Item found in Bought Items List/ Category not found");
+	            }
+	        }catch (SQLException e){
+	            e.printStackTrace();
+	            throw new BuyerException(e.getMessage());
+	        }
+	        return list;
+	}
+
+	
 
 }
